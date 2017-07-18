cf84f2a8b1d5de1d96e8f7ab44a5d27c7174ab67
Rafael Sousa (Rafao) <1c302489@gmail.com>
2017-07-18 20:24:01 -0300

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/rafao/codescanner/codescan/CodeScanActivity.java
+++ app/src/main/java/com/rafao/codescanner/codescan/CodeScanActivity.java
@@ -16,6 +16,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -48,6 +50,8 @@ public class CodeScanActivity extends AppCompatActivity implements com.rafao.cod
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_code_scan);
 
         loadComponents();
